12fece5163bd02c4c146c4384392399ecdb44f60
Cody Permann <codypermann@gmail.com>
2016-02-11 08:42:21 -0700

    First commit of Mastodon

---
+++ include/base/MastodonApp.h
@@ -0,0 +1,22 @@
+#ifndef MASTODONAPP_H
+#define MASTODONAPP_H
+
+#include "MooseApp.h"
+
+class MastodonApp;
+
+template<>
+InputParameters validParams<MastodonApp>();
+
+class MastodonApp : public MooseApp
+{
+public:
+  MastodonApp(InputParameters parameters);
+  virtual ~MastodonApp();
+
+  static void registerApps();
+  static void registerObjects(Factory & factory);
+  static void associateSyntax(Syntax & syntax, ActionFactory & action_factory);
+};
+
+#endif /* MASTODONAPP_H */
---
+++ src/base/MastodonApp.C
@@ -0,0 +1,55 @@
+#include "MastodonApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+template<>
+InputParameters validParams<MastodonApp>()
+{
+  InputParameters params = validParams<MooseApp>();
+
+  params.set<bool>("use_legacy_uo_initialization") = false;
+  params.set<bool>("use_legacy_uo_aux_computation") = false;
+  params.set<bool>("use_legacy_output_syntax") = false;
+
+  return params;
+}
+
+MastodonApp::MastodonApp(InputParameters parameters) :
+    MooseApp(parameters)
+{
+  Moose::registerObjects(_factory);
+  ModulesApp::registerObjects(_factory);
+  MastodonApp::registerObjects(_factory);
+
+  Moose::associateSyntax(_syntax, _action_factory);
+  ModulesApp::associateSyntax(_syntax, _action_factory);
+  MastodonApp::associateSyntax(_syntax, _action_factory);
+}
+
+MastodonApp::~MastodonApp()
+{
+}
+
+// External entry point for dynamic application loading
+extern "C" void MastodonApp__registerApps() { MastodonApp::registerApps(); }
+void
+MastodonApp::registerApps()
+{
+  registerApp(MastodonApp);
+}
+
+// External entry point for dynamic object registration
+extern "C" void MastodonApp__registerObjects(Factory & factory) { MastodonApp::registerObjects(factory); }
+void
+MastodonApp::registerObjects(Factory & factory)
+{
+}
+
+// External entry point for dynamic syntax association
+extern "C" void MastodonApp__associateSyntax(Syntax & syntax, ActionFactory & action_factory) { MastodonApp::associateSyntax(syntax, action_factory); }
+void
+MastodonApp::associateSyntax(Syntax & /*syntax*/, ActionFactory & /*action_factory*/)
+{
+}
