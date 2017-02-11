2e778f915af2c4618204f562db4b0805b6141661
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-02-10 17:14:14 -0700

    Update ComputeIsotropicElasticityTensorSoil to use LayeredMaterialInterface
    (refs #13)

--- include/materials/ComputeIsotropicElasticityTensorSoil.h
+++ include/materials/ComputeIsotropicElasticityTensorSoil.h
@@ -11,44 +11,43 @@
 /*                                               */
 /*     See COPYRIGHT for full restrictions       */
 /*************************************************/
-#ifndef ComputeIsotropicElasticityTensorSoil_H
-#define ComputeIsotropicElasticityTensorSoil_H
+#ifndef COMPUTEISOTROPICELASTICITYTENSORSOIL_H
+#define COMPUTEISOTROPICELASTICITYTENSORSOIL_H
 
 // MOOSE includes
 #include "ComputeElasticityTensorBase.h"
+
+// Mastodon includes
+#include "LayeredMaterialInterface.h"
 
 /**
  * This material defines elasticity tensor for isotropic linear elastic
  * layer soil material and also calculates the material properties shear and P
  * wave velocities.
  */
-class ComputeIsotropicElasticityTensorSoil : public ComputeElasticityTensorBase
+class ComputeIsotropicElasticityTensorSoil : public LayeredMaterialInterface<ComputeElasticityTensorBase>
 {
 public:
   ComputeIsotropicElasticityTensorSoil(const InputParameters & parameters);
 
 protected:
 
-  /**
-   * Populates the map of solid layer id to shear modules and density.
-   */
-  virtual void initialSetup() override;
-
   virtual void computeQpElasticityTensor() override;
 
-  /// The coupled variable field providing the layer id
-  const VariableValue & _soil_layer_variable;
+  /// Computed Youngs modules
+  Real _youngs_modulus = 0;
+
+  /// Layer parameter for the "shear_modulus" input
+  const MooseArray<Real> & _layer_shear_modulus;
+
+  /// Layer parameter for the "density" input
+  const MooseArray<Real> & _layer_density;
+
+  /// Layer parameter for the "poissons_ratio" input
+  const MooseArray<Real> & _layer_poissons_ratio;
 
   /// Flag to turn on/off P and S wave speed calculation.
   bool _wave_speed_calculation;
-
-  /// Shear Modules and Density mapping to soil id.
-  // This is being done in a single map to avoid looking up the id multipe times.
-  std::map<unsigned int, std::pair<Real, Real>> _soil_id_to_shear_modulus_and_density;
-
-  /// Young's Modules and Density mapping to soil id.
-  // This is being done in a single map to avoid looking up the id multipe times.
-  std::map<unsigned int, std::pair<Real, Real>> _soil_id_to_youngs_modulus_and_density;
 
   /// Computed shear wave speed.
   MaterialProperty<Real> * _shear_wave_speed;
@@ -58,10 +57,6 @@ protected:
 
   /// Density stored as a material property.
   MaterialProperty<Real> & _density;
-
-  /// The id to be used for looking up the shear modulus and density, it is declared once here and re-used rather
-  /// than creating a temporary variable in computeQpProperties method, simply for efficiency.
-  unsigned int _current_id;
 
   /// individual elasticity tensor
   RankFourTensor _Cijkl;
--- src/materials/ComputeIsotropicElasticityTensorSoil.C
+++ src/materials/ComputeIsotropicElasticityTensorSoil.C
@@ -17,19 +17,21 @@ template<>
 InputParameters validParams<ComputeIsotropicElasticityTensorSoil>()
 {
   InputParameters params = validParams<ComputeElasticityTensorBase>();
+  params += validParams<LayeredMaterialInterface<>>();
   params.addClassDescription("Compute an isotropic elasticity tensor for a layered soil material when shear modulus, poisson's ratio and density are provided as input for each layer.");
-  params.addRequiredParam<std::vector<unsigned int>>("layer_ids", "Vector of layer ids that map one-to-one with the 'shear_modulus', 'poissons_ratio' and 'density' input parameters.");
+  params.setDocString("layer_ids", "Vector of layer ids that map one-to-one with the 'shear_modulus', 'poissons_ratio' and 'density' input parameters.");
   params.addRequiredParam<std::vector<Real>>("poissons_ratio", "Vector of Poisson's ratio values that map one-to-one with the number 'layer_ids' parameter.");
   params.addRequiredParam<std::vector<Real>>("shear_modulus", "Vector of  shear modulus values that map one-to-one with the number 'layer_ids' parameter.");
   params.addRequiredParam<std::vector<Real>>("density", "Vector of density values that map one-to-one with the number 'layer_ids' parameter.");
-  params.addRequiredCoupledVar("layer_variable", "The variable providing the soil layer identification.");
   params.addParam<bool>("wave_speed_calculation", true, "Set to False to turn off P and S wave speed calcualtion.");
   return params;
 }
 
 ComputeIsotropicElasticityTensorSoil::ComputeIsotropicElasticityTensorSoil(const InputParameters & parameters) :
-    ComputeElasticityTensorBase(parameters),
-    _soil_layer_variable(coupledValue("layer_variable")),
+    LayeredMaterialInterface(parameters),
+    _layer_shear_modulus(getLayerParam<Real>("shear_modulus")),
+    _layer_density(getLayerParam<Real>("density")),
+    _layer_poissons_ratio(getLayerParam<Real>("poissons_ratio")),
     _wave_speed_calculation(getParam<bool>("wave_speed_calculation")),
     _shear_wave_speed(_wave_speed_calculation ? &declareProperty<Real>("shear_wave_speed") : NULL),
     _P_wave_speed(_wave_speed_calculation ? &declareProperty<Real>("P_wave_speed") : NULL),
@@ -41,57 +43,25 @@ ComputeIsotropicElasticityTensorSoil::ComputeIsotropicElasticityTensorSoil(const
 }
 
 void
-ComputeIsotropicElasticityTensorSoil::initialSetup()
-{
-  // Get the vectors for poulating the id to the modules/density map
-  const std::vector<unsigned int> & layers = getParam<std::vector<unsigned int>>("layer_ids");
-  const std::vector<Real> & modulus = getParam<std::vector<Real>>("shear_modulus");
-  const std::vector<Real> & density = getParam<std::vector<Real>>("density");
-  const std::vector<Real> & poissons_ratio = getParam<std::vector<Real>>("poissons_ratio");
-
-  // Check that the vectors are all the same size
-  if (layers.size() != modulus.size() || layers.size() != density.size() || modulus.size() != density.size() || modulus.size() != poissons_ratio.size())
-    mooseError("The 'layer_ids', 'shear_modulus', 'poissons_ratio' and 'density' vectors must be the same size.");
-
-  // Build the map
-  for (unsigned int i = 0; i < layers.size(); ++i)
-  {
-    _soil_id_to_shear_modulus_and_density[layers[i]] = std::pair<Real, Real>(modulus[i], density[i]);
-    _soil_id_to_youngs_modulus_and_density[layers[i]] = std::pair<Real, Real>(modulus[i] * 2.0* (1.0 + poissons_ratio[i]), density[i]);
-  }
-}
-
-void
 ComputeIsotropicElasticityTensorSoil::computeQpElasticityTensor()
 {
-  // Determine the current id. The variable which is a Real must be converted to a unsigned int for lookup, so first
-  // it is rounded to avoid Real values that are just below the desired value.
-  _current_id = static_cast<unsigned int>(std::round(_soil_layer_variable[_qp]));
 
-  // Locate the shear modulus and density values for the current soil id and error if it is not found
-  std::map<unsigned int, std::pair<Real, Real>>::iterator it_s = _soil_id_to_shear_modulus_and_density.find(_current_id);
-  if (it_s == _soil_id_to_shear_modulus_and_density.end())
-    mooseError("The soil id variable value (" << _current_id << ") was not provided in the 'layer_ids' parameter.");
+  _youngs_modulus = _layer_shear_modulus[_qp] * 2.0* (1.0 + _layer_poissons_ratio[_qp]);
 
-  // Locate the Young's modulus and density values for the current soil id and error if it is not found
-  std::map<unsigned int, std::pair<Real, Real>>::iterator it_p = _soil_id_to_youngs_modulus_and_density.find(_current_id);
-  if (it_p == _soil_id_to_youngs_modulus_and_density.end())
-    mooseError("The soil id variable value (" << _current_id << ") was not provided in the 'layer_ids' parameter.");
+  _density[_qp] = _layer_density[_qp];
 
   if (_wave_speed_calculation)
   {
     // Shear wave speed: sqrt(G/rho)
-    (*_shear_wave_speed)[_qp] = std::sqrt(it_s->second.first / it_s->second.second);
+    (*_shear_wave_speed)[_qp] = std::sqrt(_layer_shear_modulus[_qp]/ _density[_qp]);
 
     // P wave speed: sqrt(E/rho)
-    (*_P_wave_speed)[_qp] = std::sqrt(it_p->second.first / it_p->second.second);
+    (*_P_wave_speed)[_qp] = std::sqrt(_youngs_modulus / _density[_qp]);
   }
 
-  _density[_qp] = it_p->second.second;
-
   std::vector<Real> iso_const(2);
-  iso_const[0] = it_s->second.first * (it_p->second.first - 2.0 * it_s->second.first) / (3.0 * it_s->second.first - it_p->second.first); // lambda = G*(E-2G)/(3G-E)
-  iso_const[1] = it_s->second.first; // shear modulus
+  iso_const[0] = _layer_shear_modulus[_qp] * (_youngs_modulus - 2.0 * _layer_shear_modulus[_qp]) / (3.0 * _layer_shear_modulus[_qp] - _youngs_modulus); // lambda = G*(E-2G)/(3G-E)
+  iso_const[1] = _layer_shear_modulus[_qp]; // shear modulus
 
   //Fill elasticity tensor
   _Cijkl.fillFromInputVector(iso_const, RankFourTensor::symmetric_isotropic);
