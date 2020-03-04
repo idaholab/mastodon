e939bd7f9bb633fa3f1db48203a204687eb59a0e
Hugo Nie <gnie@ncsu.edu>
2020-03-04 11:58:06 -0500

    rename by capital letter

--- include/utils/solver.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SOLVER_H
-#define _SOLVER_H
-
-#define SOLVER_STOP_OBJECTIVE 1e-11
-
-void solveLnParams( std::vector<double> x, std::vector<double> y, double& a, double& b ) {return;} 
-
-#endif // _SOLVER_H
